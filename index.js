e6fe64ec5f45c0bc31fb55e50ce393edb1d982f8
Digices LLC <digices@mp5.local>
2018-09-13 13:07:45 -0600

    Create vanilla express server

--- index.js
+++ index.js
@@ -9,4 +9,23 @@
  * @version   0.1.0
  * @since     0.1.0
  */
- 
+
+// Require Modules
+
+const express = require('express');
+
+// Create the Server
+
+const server = express();
+
+// Define Routes
+
+server.get('/', (request, response) => {
+  response.send('<h1>☕️ Hello Express</h1>');
+});
+
+// Start the Server
+
+server.listen(4000, () => {
+  console.log('Server listening at http://localhost:4000');
+});
