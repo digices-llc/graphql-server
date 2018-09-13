e861782d567ce4d807fa6578c0d446264895614e
Digices LLC <digices@mp5.local>
2018-09-13 12:57:42 -0600

    NPM init with main entry point

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * GraphQL Server
+ *
+ * @file      index
+ * @url       https://github.com/digices-llc/graphql.git
+ * @author    Roderic Linguri <linguri@digices.com>
+ * @copyright 2018 Digices LLC
+ * @license   MIT
+ * @version   0.1.0
+ * @since     0.1.0
+ */
+ 
